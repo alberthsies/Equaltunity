0131d5d3c5b8c3b9e0e2fea090c0cd7b7228150c
Yuan-Shen, Hsieh <alberthsies@gmail.com>
2022-02-05 18:07:54 -0500

    Add mobile login page

---
+++ mobile/Equaltunity/Equaltunity/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Equaltunity
+//
+//  Created by 謝沅伸 on 2022/2/5.
+//  Copyright © 2022 Yuan-Shen Hsieh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        VStack {
+            Image("logo")
+                .resizable()
+                .frame(width: 120.0, height: 120.0)
+                .clipShape(Circle())
+                .shadow(radius: 7)
+            
+            Text("Equaltunity")
+                .font(.title).multilineTextAlignment(.leading)
+            
+            TextField("Username", text: $username)
+                .frame(height: 40.0)
+                .disableAutocorrection(true)
+                .padding(5)
+                
+            TextField("Password", text: $password)
+                .frame(height: 40.0)
+                .disableAutocorrection(true)
+                .padding(5)
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Login")
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(80)
+                .foregroundColor(.white)
+                .padding(3)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                    .stroke(Color.blue, lineWidth: 3)
+                )
+            }
+        }.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
